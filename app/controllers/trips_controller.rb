e90898efd2473f7843743f7891bb5f9db1efabf6
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-11-17 21:23:02 +0900

    createコントローラーを記述

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,14 @@ class TripsController < ApplicationController
   def new
     @trip = Trip.new
   end
+
+  def create
+    Trip.create(trip_params)
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "trips#index"
-  resources :trips, only: [:index, :new]
+  resources :trips, only: [:index, :new, :create]
 end
