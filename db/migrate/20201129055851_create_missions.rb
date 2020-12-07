5aac3686eb25b078477a4c71e4b9babc442c90e9
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-12-07 20:23:37 +0900

    マイグレーションファイルを修正

--- db/migrate/20201129055851_create_missions.rb
+++ db/migrate/20201129055851_create_missions.rb
@@ -2,7 +2,6 @@ class CreateMissions < ActiveRecord::Migration[6.0]
   def change
     create_table :missions do |t|
       t.string :name, null: false
-      t.boolean :checked
       t.references  :trip,  null: false, foreign_key: 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2020_11_29_055851) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "number", null: false
     t.bigint "trip_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -23,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_11_29_055851) do
 
   create_table "missions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "trip_id", null: false
