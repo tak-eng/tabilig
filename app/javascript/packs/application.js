b095b02c86f3113f564f90f1e425eefcef598f58
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-12-07 20:30:43 +0900

    不必要なJSファイルを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../item")
 require("../todo")
-// require("../checked")
-// require("../memo")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
