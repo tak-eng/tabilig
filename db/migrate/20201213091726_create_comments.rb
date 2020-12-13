6ceb9766fc212c3d835b4ba3e66d7e69d4a2521d
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-12-13 18:59:56 +0900

    commentのファイルを削除

--- db/migrate/20201213091726_create_comments.rb
+++
@@ -1,9 +0,0 @@
-class CreateComments < ActiveRecord::Migration[6.0]
-  def change
-    create_table :comments do |t|
-      t.text :text, null: false
-      t.references  :trip,  null: false, foreign_key: 
-      t.timestamps
-    end
-  end
-end
